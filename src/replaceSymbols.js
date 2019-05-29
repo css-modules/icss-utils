9ce05e05250b15d66c2ceba0c569f33c4d9c90e5
evilebottnawi <sheo13666q@gmail.com>
2019-05-29 17:33:50 +0300

    fix: handle cases when properties `undefined` in `replaceSymbols`

--- src/replaceSymbols.js
+++ src/replaceSymbols.js
@@ -2,12 +2,13 @@ import replaceValueSymbols from "./replaceValueSymbols.js";
 
 const replaceSymbols = (css, replacements) => {
   css.walk(node => {
-    if (node.type === "decl") {
+    if (node.type === "decl" && node.value) {
       node.value = replaceValueSymbols(node.value, replacements);
-    } else if (node.type === "rule") {
+    } else if (node.type === "rule" && node.selector) {
       node.selector = replaceValueSymbols(node.selector, replacements);
     } else if (
       node.type === "atrule" &&
+      node.params &&
       ["media", "supports"].includes(node.name.toLowerCase())
     ) {
       node.params = replaceValueSymbols(node.params, replacements);
